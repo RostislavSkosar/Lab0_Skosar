d399b4528c7b3eeb949107367258a15a826ec2d3
RostislavSkosar <112941716+RostislavSkosar@users.noreply.github.com>
2023-12-06 14:54:59 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cout << "Hello World!\n";
+    cout << "enter character\n";
+    cin >> a;
+    cout << a;
+    return 1;
+}
